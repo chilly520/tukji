ea39a94c315cad5f80207db4a1cadfe7ff5340ac
乔篱 <by811520@gmail.com>
2025-10-23 11:49:16 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'ephone-v1.0';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  // 添加其他需要缓存的资源
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('缓存已打开');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
